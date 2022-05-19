5e737e1a4cbbc76d3ea7f2e3f66cc6a5f4d674db
Ostap Blystsiv <ostapcr7@gmail.com>
2022-05-19 22:36:15 +0300

    add

--- my.js
+++ my.js
@@ -112,3 +112,70 @@
 // console.log(getSpeedStatistic([10]));
 // console.log(getSpeedStatistic([]));
 
+// =============================
+
+// function compareRobots(firstRobotResults, secondRobotResults) {
+// 	if (
+// 		firstRobotResults.reduce((partialSum, a) => partialSum + a, 0) >
+// 		secondRobotResults.reduce((partialSum, a) => partialSum + a, 0)
+// 	) {
+// 		return 'First robot for sale!';
+// 	} else if (
+// 		firstRobotResults.reduce((partialSum, a) => partialSum + a, 0) <
+// 		secondRobotResults.reduce((partialSum, a) => partialSum + a, 0)
+// 	) {
+// 		return 'Second robot for sale!';
+// 	} else {
+// 		return 'Both robots for sale!';
+// 	}
+// }
+
+// console.log(compareRobots([9, 7, 9], [1, 3, 4, 5, 12]));
+// console.log(compareRobots([1, 3, 4], [1, 1, 4, 5]));
+
+// ============================
+
+// function isSorted(boxNumbers) {
+// 	for (let i = 0; i < boxNumbers.length; i++) {
+// 		if (boxNumbers.length === 0) {
+// 			return false;
+// 		} else if (boxNumbers[i] < boxNumbers[i - 1]) {
+// 			return false;
+// 		}
+// 	}
+// 	return true;
+// }
+
+// console.log(isSorted([1, 2, 3, 4, 5]));
+// console.log(isSorted([0, 3, 1, 2, 2, 2]));
+
+// ============================
+
+// function createArray(N) {
+// 	let arr = [];
+// 	for (let i = 1; i <= N; i++) {
+// 		arr.push(i);
+// 	}
+// 	return arr;
+// }
+
+// ============================
+
+// function getPlan(startProduction, numberOfMonths, percent) {
+
+// }
+// console.log(getPlan(200, 3, 50));
+// console.log(getPlan(10, 4, 30));
+
+// ============================
+
+function getLocation(coordinates, commands) {
+	let arr = coordinates.slice();
+	commands.forEach((cmd) => {
+		arr[0] += cmd === 'left' ? -1 : cmd === 'right' ? 1 : 0;
+		arr[1] += cmd === 'back' ? -1 : cmd === 'forward' ? 1 : 0;
+	});
+	return arr;
+}
+
+console.log(getLocation([0, 0], ['forward', 'right']));
